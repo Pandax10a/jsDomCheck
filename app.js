ba8643dba774c2265432e518c8be4582cbdd429b
panda1 <blueblue0011@gmail.com>
2022-07-14 18:01:15 -0700

    last part not working

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ let all_class_tag = document.querySelectorAll(`.moveright`);
 for (let i = 0; i < all_class_tag.length; i++) {
     all_class_tag[i][`innerText`] = `grabbed by my class`;
 }
+
+
+let header = document.getElementById(`header2`);
+header[`innerText`] = `Something id`;
+
+let footer = document.getElementById(`footer2`);
+footer[`innerText`] = `Something footer id`;
+
+let middle = document.getElementsByClassName(`theone`);
+let counter = 0;
+while (counter < middle.length) {
+    middle[`innerText`] = `grabbed by my class, differently`;
+    counter++;
+}
